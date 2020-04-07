38f57d1f1940c72d6ba17ba23eb98ebf361475dc
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-04-07 18:56:16 +0300

    Add files via upload

--- 1.Svn/1.Server/game/src/packet.h
+++ 1.Svn/1.Server/game/src/packet.h
@@ -12,9 +12,9 @@
 	
 ///Add
 #ifdef ENABLE_RENEWAL_SHOPEX
-	DWORD 	price_type;
-	DWORD 	price_vnum;
-	packet_shop_item() : price_type(1), price_vnum(0) {
+	DWORD 	price_type = 1;
+	DWORD 	price_vnum = 0;
+	packet_shop_item() {
 		memset(&alSockets, 0, sizeof(alSockets));
 		memset(&aAttr, 0, sizeof(aAttr));
 	}
